1d1ef0e6e6b0d814312144ecc2c43d843c77f267
Morenike Oyewole <herroyalpianist@gmail.com>
2021-04-11 12:02:41 +0100

    added responsiveness

---
+++ script.js
@@ -0,0 +1,7 @@
+const myToggler = document.getElementsByClassName('toggler');
+const navBarToggle = document.getElementsByClassName('nav-links');
+
+myToggle.addEventListener('click', function () {
+    
+    myToggler.classList.toggle('active');
+});
